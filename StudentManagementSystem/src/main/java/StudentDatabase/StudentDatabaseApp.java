26da59059b21896eb365678a25ff9bd29c30f144
Jeff White <ourownkey@gmail.com>
2021-01-03 14:31:09 -0500

    Added functionality for adding a new student and returning a unique student ID

--- StudentManagementSystem/src/main/java/StudentDatabase/StudentDatabaseApp.java
+++ StudentManagementSystem/src/main/java/StudentDatabase/StudentDatabaseApp.java
@@ -3,6 +3,7 @@ package StudentDatabase;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
+		Student student1 = new Student();
 		// TODO Auto-generated method stub
 
 	}
